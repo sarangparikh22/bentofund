97a6cdd3e16a26d4715f3204b4d48556003bd9e3
Sarang Parikh <sarangparikh22@gmail.com>
2022-03-28 03:36:30 +0400

    unsol

--- contracts/BentoFund.sol
+++ contracts/BentoFund.sol
@@ -26,7 +26,8 @@ contract BentoFund is IBentoFund, BoringOwnable, BoringBatchable {
         bentoBox = _bentoBox;
         wETH = _wETH;
         fundIds = 1;
-        _bentoBox.registerProtocol();
+
+        // registed with bentobox
     }
 
     function setBentoBoxApproval(
@@ -36,14 +37,7 @@ contract BentoFund is IBentoFund, BoringOwnable, BoringBatchable {
         bytes32 r,
         bytes32 s
     ) external override {
-        bentoBox.setMasterContractApproval(
-            user,
-            address(this),
-            approved,
-            v,
-            r,
-            s
-        );
+        
     }
 
     function createProject(
@@ -52,21 +46,7 @@ contract BentoFund is IBentoFund, BoringOwnable, BoringBatchable {
         uint64 start,
         uint64 end
     ) external override returns (uint256 fundId) {
-        require(start > block.timestamp, "BentoFund: Invalid Start Time");
-        require(end > start, "BentoFund: Invalid End Time");
-
-        fundId = fundIds++;
-
-        funds[fundId] = Project({
-            owner: msg.sender,
-            token: token,
-            goal: goal,
-            depositedShares: 0,
-            start: start,
-            end: end
-        });
-
-        emit LogCreateProject(fundId, msg.sender, token, goal, start, end);
+        
     }
 
     function fundProject(
@@ -74,27 +54,7 @@ contract BentoFund is IBentoFund, BoringOwnable, BoringBatchable {
         uint128 amount,
         bool fromBentoBox
     ) external payable override returns (uint128 depositedShares) {
-        Project storage project = funds[fundId];
-        require(
-            block.timestamp >= project.start,
-            "BentoFund: Project Not Started"
-        );
-        require(project.end > block.timestamp, "BentoFund: Project ended");
-
-        depositedShares = uint128(
-            _depositToken(
-                address(project.token),
-                msg.sender,
-                address(this),
-                amount,
-                fromBentoBox
-            )
-        );
-
-        project.depositedShares += depositedShares;
-        funders[fundId][msg.sender] += depositedShares;
-
-        emit LogFundProject(fundId, amount);
+        
     }
 
     function refund(uint256 fundId, bool toBentoBox)
@@ -102,27 +62,7 @@ contract BentoFund is IBentoFund, BoringOwnable, BoringBatchable {
         override
         returns (uint128 amount)
     {
-        Project storage project = funds[fundId];
-
-        require(block.timestamp > project.end, "BentoFund: Project not ended");
-
-        require(
-            project.depositedShares < project.goal,
-            "BentoFund: Project funding success"
-        );
-
-        amount = funders[fundId][msg.sender];
-        funders[fundId][msg.sender] = 0;
-
-        _transferToken(
-            address(project.token),
-            address(this),
-            msg.sender,
-            amount,
-            toBentoBox
-        );
-
-        emit LogRefund(fundId, msg.sender);
+        
     }
 
     function withdraw(uint256 fundId, bool toBentoBox)
@@ -130,71 +70,8 @@ contract BentoFund is IBentoFund, BoringOwnable, BoringBatchable {
         override
         returns (uint128 amount)
     {
-        Project storage project = funds[fundId];
-
-        require(block.timestamp > project.end, "BentoFund: Project not ended");
-
-        require(
-            project.depositedShares >= project.goal,
-            "BentoFund: Project funding failed"
-        );
-
-        amount = project.depositedShares;
-        project.depositedShares = 0;
-
-        _transferToken(
-            address(project.token),
-            address(this),
-            project.owner,
-            amount,
-            toBentoBox
-        );
-
-        emit LogWithdraw(fundId, msg.sender);
+       
     }
 
-    function _depositToken(
-        address token,
-        address from,
-        address to,
-        uint256 amount,
-        bool fromBentoBox
-    ) internal returns (uint256 depositedShares) {
-        if (token == wETH && address(this).balance >= amount) {
-            (, depositedShares) = bentoBox.deposit{value: amount}(
-                address(0),
-                from,
-                to,
-                amount,
-                0
-            );
-        } else {
-            if (fromBentoBox) {
-                depositedShares = bentoBox.toShare(token, amount, false);
-                bentoBox.transfer(token, from, to, depositedShares);
-            } else {
-                (, depositedShares) = bentoBox.deposit(
-                    token,
-                    from,
-                    to,
-                    amount,
-                    0
-                );
-            }
-        }
-    }
-
-    function _transferToken(
-        address token,
-        address from,
-        address to,
-        uint256 amount,
-        bool toBentoBox
-    ) internal {
-        if (toBentoBox) {
-            bentoBox.transfer(token, from, to, amount);
-        } else {
-            bentoBox.withdraw(token, from, to, 0, amount);
-        }
-    }
+    
 }
